6da74a4e4f9477078fb85a8a8bc2da4a47c157d6
Petra Rebrosova <peta.rebrosova@gmail.com>
2024-05-14 21:56:40 +0200

    Retrieve source and target files from console inputs

--- RwsMoraviaHomework/Program.cs
+++ RwsMoraviaHomework/Program.cs
@@ -7,34 +7,36 @@ namespace Moravia.Homework
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            // Define path to the sorce and targer files
-            //var relativeSourceFilePath = "..\\..\\..\\SourceFiles\\Document1.xml";
-            //var relativeTargetFilePath = "..\\..\\..\\TargetFiles\\Document1.json";
-            var sourceStorage = SupportedStorages.FileSystem; // skusit rozpoznat
-            var targetStorage = SupportedStorages.FileSystem;
-            var relativeSourceFilePath = "..\\..\\..\\..\\XmlInput.xml";
-            var relativeTargetFilePath = "..\\..\\..\\..\\JsonOutput.json";
+            Console.WriteLine("Define path to the source file.");
+            var relativeSourceFilePath = Console.ReadLine()?.Trim() ?? string.Empty;
+            Console.WriteLine($"Define storage for source. {SupportedStorages.GetDescription()}");
+            var sourceStorage = Console.ReadLine()?.Trim().ToLower() ?? string.Empty;
+
+            Console.WriteLine("Define path to the target file.");
+            var relativeTargetFilePath = Console.ReadLine()?.Trim() ?? string.Empty;
+            Console.WriteLine($"Define storage for target. {SupportedStorages.GetDescription()}");
+            var targetStorage = Console.ReadLine()?.Trim().ToLower() ?? string.Empty;
 
             var sourceFileName = Path.Combine(Environment.CurrentDirectory, relativeSourceFilePath);
             var targetFileName = Path.Combine(Environment.CurrentDirectory, relativeTargetFilePath);
 
-            // TODO: var source = zistit z kade ide subor
-
-            var reader = ReaderFactory.CreateFileReader(sourceFileName, sourceStorage);
-            var writer = WriterFactory.CreateFileWriter(targetFileName, targetStorage);
-            var converter = ConverterFactory.CreateConverter(sourceFileName, reader, writer);
-
             try
             {
+                var reader = ReaderFactory.CreateFileReader(sourceFileName, sourceStorage);
+                var writer = WriterFactory.CreateFileWriter(targetFileName, targetStorage);
+                var converter = ConverterFactory.CreateConverter(sourceFileName, reader, writer);
+
                 var targetFileExtention = FileExtentionUtils.GetFileExtention(targetFileName);
                 converter.Convert(targetFileExtention);
             }
             catch (Exception ex)
             {
-                throw new Exception(ex.Message);
+                WriteErrorMessage(ex.Message);
             }
         }
+
+        private static void WriteErrorMessage(string message) => Console.Error.WriteLine(message);
     }
 }
